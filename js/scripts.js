fe9e864702cec90710fa4ac9b4c36692522ed10a
Brevin Cronk and Tiffany Siu <brevin@>
2020-01-21 09:39:54 -0800

    add arrays and unordered list

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,23 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event){
     var arr = [];
-    var input1 = $("#input1").val();
-    arr.push(input1);
+    var input1 = $("#inputOne").val();
+    var input2 = $("#inputTwo").val();
+    var input3 = $("#inputThree").val();
+    var input4 = $("#inputFour").val();
+    arr.push(input1, input2, input3, input4);
+    var newArray=[];
+    newArray.push(arr[0], arr[1], arr[2]);
+
     event.preventDefault();
     
     console.log(arr);
+    console.log(newArray);
+
+    $(".thing1").append(newArray[0]);
+    $(".thing2").append(newArray[1]);
+    $(".thing3").append(newArray[2]);
+
   });
 
 });
